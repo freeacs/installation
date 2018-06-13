9dde10767a69262168abcf1559b1a9ad53e44702
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-06-13 21:49:06 +0200

    settings

--- ubuntu/tasks/03_install_mysql_server.sh
+++ ubuntu/tasks/03_install_mysql_server.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 function install_mysql_server_init() {
-   task_setup "install_mysql_server" "MySQLInstall" "Install MySQL"
+  task_setup "install_mysql_server" "MySQLInstall" "Install MySQL"
+  settings_init ".settings"
 }
 
 function install_mysql_server_run() {
