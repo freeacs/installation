d3fb9e7833198b3c0aae88c75e7f141a6499f002
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-06-13 21:40:07 +0200

    simplify

--- ubuntu/tasks/03_install_mysql_server.sh
+++ ubuntu/tasks/03_install_mysql_server.sh
@@ -7,24 +7,13 @@ function install_mysql_server_init() {
 function install_mysql_server_run() {
   local rootPass
   if type mysql >/dev/null 2>&1; then
+    rootPass=$(enter_variable_hidden "Enter mysql root password")
+  else
     rootPass=$(pwmake 128)
-    apt-get -y remove --purge 'mysql*'
     echo "mysql-server-5.7 mysql-server/root_password password root" | debconf-set-selections
     echo "mysql-server-5.7 mysql-server/root_password_again password root" | debconf-set-selections
     apt-get -y install mysql-server-5.7
-    echo -e "root\nn\nY\nY\nY\nY\n" | mysql_secure_installation
-    mysql -u root --password="$rootPass" -e <<-EOSQL
-      DELETE FROM mysql.user WHERE User='';
-      DROP DATABASE IF EXISTS test;
-      DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-      DELETE FROM mysql.user where user != 'mysql.sys';
-      CREATE USER 'root'@'%' IDENTIFIED BY '${rootPass}';
-      GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
-      FLUSH PRIVILEGES;
-EOSQL
-    systemctl restart mysql
-  else
-    rootPass=$(enter_variable_hidden "Enter mysql root password")
+    mysql -u root --password="root" password $rootPass
   fi
   settings_set "mysql_root_password" ${rootPass}
 }
